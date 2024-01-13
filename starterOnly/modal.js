0b5aaf05ccf4ad072e233d995021614e343e0fc6
Frédérique <tarroux.frederique@gmail.com>
2024-01-13 17:23:18 +0100

    #1 Fermeture modal opérationnelle

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,7 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  // modification de la déclaration de la variable :
+  // var changé par let
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,6 +13,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelectorAll(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +21,14 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal event
+close.forEach((span) => span.addEventListener("click", closeModal));
+
+// close modal form 
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
