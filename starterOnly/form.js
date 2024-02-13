fda246cb90e6d1c85cd898ac7e0732b3b1b60613
Frédérique <tarroux.frederique@gmail.com>
2024-02-13 19:17:02 +0100

    Envoie fonctionnalité radio avant modif avec Steve

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,154 +1,169 @@
-
-let form = document.querySelector('form');
+let form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
-  
-  const firstName = document.getElementById('first');
-  const lastName = document.getElementById('last');
-  const mail = document.getElementById('email');
-  const birthDate = document.getElementById('birthdate');
-  const quantity = document.getElementById('quantity');
+  const firstName = document.getElementById("first");
+  const lastName = document.getElementById("last");
+  const mail = document.getElementById("email");
+  const birthDate = document.getElementById("birthdate");
+  const quantity = document.getElementById("quantity");
   const radio = document.querySelectorAll('input[type="radio"]');
-  const cgeneral = document.getElementById('checkbox1');
-  const newevent = document.getElementById('checkbox2');
-  console.log(document.querySelectorAll('input'));
-  
+  const cgeneral = document.getElementById("checkbox1");
+  const newevent = document.getElementById("checkbox2");
+  console.log(document.querySelectorAll("input"));
+
   let valeur;
-  for(let i = 0; i < radio.length; i++){ // voir pour modifier avec forEach et map 
-  if(radio[i].checked) {
-  valeur = radio[i].value;
+  for (let i = 0; i < radio.length; i++) {
+    // voir pour modifier avec forEach et map
+    if (radio[i].checked) {
+      valeur = radio[i].value;
     }
   }
   console.log(valeur);
 
-
-  if(newevent.checked) {
+  if (newevent.checked) {
     console.log(newevent.value);
   }
-  
 
-  if(ctrlForm(firstName, lastName, mail, birthDate, quantity, valeur, cgeneral, newevent)) {
-    alert('Merci ! Votre réservation a été reçue.');
+  if (
+    ctrlForm(
+      firstName,
+      lastName,
+      mail,
+      birthDate,
+      quantity,
+      valeur,
+      cgeneral,
+      newevent
+    )
+  ) {
+    alert("Merci ! Votre réservation a été reçue.");
   } else {
     event.preventDefault();
-    alert('error');
+    alert("error");
   }
 });
 
-function ctrlForm(firstName, lastName, mail, birthDate, quantity, valeur, cgeneral/*newevent*/) {
+function ctrlForm(
+  firstName,
+  lastName,
+  mail,
+  birthDate,
+  quantity,
+  valeur,
+  cgeneral /*newevent*/
+) {
   let error;
 
   //FIRST NAME
-  if(firstName.value.length < 2) {
-    console.log('erreur first');
+  if (firstName.value.length < 2) {
+    console.log("erreur first");
     errorFirst = true;
   } else {
     errorFirst = false;
   }
 
   // LAST NAME
-  if(lastName.value.length < 2) {
+  if (lastName.value.length < 2) {
     errorLast = true;
-    console.log('erreur last');
+    console.log("erreur last");
   } else {
     errorLast = false;
   }
 
   // MAIL
-  if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value)) {
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value)) {
     errorMail = true;
-    console.log('erreur mail');
+    console.log("erreur mail");
   } else {
     errorMail = false;
   }
-  
-  // BIRTHDATE 
-  if(isNaN(new Date(birthDate.value))) {
+
+  // BIRTHDATE
+  if (isNaN(new Date(birthDate.value))) {
     errorbirthdate = true;
-    console.log('erreur annif');
+    console.log("erreur annif");
   } else {
     errorbirthdate = false;
   }
 
   // QUANTITY
-  if(quantity.value === null && isNaN(quantity.value)) {
-    console.log('erreur quantity value');
+  if (quantity.value === null && isNaN(quantity.value)) {
+    console.log("erreur quantity value");
   }
 
   // RADIO BTN
   switch (valeur) {
-    case 'New York':
+    case "New York":
       break;
-    case 'San Francisco':
+    case "San Francisco":
       break;
-    case 'Seattle':
+    case "Seattle":
       break;
-    case 'Chicago':
+    case "Chicago":
       break;
-    case 'Boston':
+    case "Boston":
       break;
-    case 'Portland':
+    case "Portland":
       break;
     default:
       console.log(valeur);
-}
+  }
 
   // CHECKBOX CONDITION GENERALE
-  if(cgeneral.checked === false) {
+  if (cgeneral.checked === false) {
     errorCGeneral = true;
   } else {
     errorCGeneral = false;
   }
 
   // validate
-  if(errorFirst) {
-    document.getElementById('errorFirst').innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
+  if (errorFirst) {
+    document.getElementById("errorFirst").innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
     return false;
   } else {
-    document.getElementById('errorFirst').innerHTML = '';
+    document.getElementById("errorFirst").innerHTML = "";
   }
 
-  if(errorLast) {
-    document.getElementById('errorLast').innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
+  if (errorLast) {
+    document.getElementById("errorLast").innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom";
     return false;
   } else {
-    document.getElementById('errorLast').innerHTML = '';
-    
+    document.getElementById("errorLast").innerHTML = "";
   }
 
-  if(errorMail) {
-    document.getElementById('errorMail').innerHTML = 'Veuillez corriger votre adresse mail';
+  if (errorMail) {
+    document.getElementById("errorMail").innerHTML =
+      "Veuillez corriger votre adresse mail";
     return false;
   } else {
-    document.getElementById('errorMail').innerHTML = '';
-    
+    document.getElementById("errorMail").innerHTML = "";
   }
 
-  if(errorbirthdate) {
-    document.getElementById('errorBirthdate').innerHTML = 'Veuillez renseigner votre date de naissance';
+  if (errorbirthdate) {
+    document.getElementById("errorBirthdate").innerHTML =
+      "Veuillez renseigner votre date de naissance";
     return false;
   } else {
-    document.getElementById('errorBirthdate').innerHTML = '';
-    
+    document.getElementById("errorBirthdate").innerHTML = "";
   }
 
-  if(errorTournois) {
-    document.getElementById('errorTournois').innerHTML = 'Veuillez choisir le tournois auquel vous souhaitez participer';
+  if (errorTournois) {
+    document.getElementById("errorTournois").innerHTML =
+      "Veuillez choisir le tournois auquel vous souhaitez participer";
     return false;
   } else {
-    document.getElementById('errorTournois').innerHTML = '';
-    
+    document.getElementById("errorTournois").innerHTML = "";
   }
 
-  if(errorCGeneral) {
-    document.getElementById('errorCGeneral').innerHTML = 'Veuillez accepter nos conditions générales';
+  if (errorCGeneral) {
+    document.getElementById("errorCGeneral").innerHTML =
+      "Veuillez accepter nos conditions générales";
     return false;
   } else {
-    document.getElementById('errorCGeneral').innerHTML = '';
+    document.getElementById("errorCGeneral").innerHTML = "";
   }
 
   return true;
-  
 }
-
-
--- starterOnly/form2.js
+++ starterOnly/form2.js
@@ -1,183 +1,160 @@
+const form = document.getElementById("form");
 
-const form = document.getElementById('form');
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //   const errorMessages = {
+  //     first: (errorFirst.innerHTML =
+  //       "Veuillez entrer 2 caractères ou plus pour le champ du prénom"),
+  //     last: (errorLast.innerHTML =
+  //       "Veuillez entrer 2 caractères ou plus pour le champ du nom"),
+  //     email: (errorMail.innerHTML = "Veuillez corriger votre adresse mail"),
+  //     birthdate: (errorBirthdate.innerHTML =
+  //       "Veuillez renseigner votre date de naissance"),
+  //     location: (errorTournois.innerHTML =
+  //       "Veuillez choisir le tournois auquel vous souhaitez participer"),
+  //     CGeneral: (errorCGeneral.innerHTML =
+  //       "Veuillez accepter nos conditions générales"),
+  //   };
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+  console.log(spanError);
 
-    // Objet pour stocker les données du formulaire
-    const formElement = {}; 
+  // => Objet pour stocker les données du formulaire :
 
-    // Parcourir chaque champ du formulaire
-    // Type text : Nom, Prenom, Mail
-    form.querySelectorAll('input').forEach(function(champ) {
-        // Stocker la valeur de chaque champ dans l'objet 
-        formElement[champ.name] = champ.value; 
-        console.log(formElement);
-    });
+  // ##radioInputs :
+  const radioInputs = document.querySelectorAll('input[type="radio"]');
 
-    console.log(formElement);
-    if(ctrlForm(formElement)) {
-        closeModal();
-        afficherToast();
-    } else {
-        alertError();
+  // ##formElement : All values
+  const formElement = {
+    first: "",
+    last: "",
+    email: "",
+    birthdate: "",
+    quantity: "",
+    location: [radioInputs],
+    CGeneral: true,
+    futurTournois: false,
+  };
+
+  //console.log(inputChecked);
+  console.log(radioInputs);
+  console.log(location);
+
+  // Parcourir chaque champ du formulaire
+  form.querySelectorAll("input").forEach(function (champ) {
+    // Stocker la valeur de chaque champ dans l'objet
+    formElement[champ.name] = champ.value;
+  });
+
+  // variable permettant de stocker input checked
+  let inputChecked = null;
+  //trouver l'input checked
+  radioInputs.forEach(function (input) {
+    if (input.checked) {
+      inputChecked = input.value;
+      formElement.location = inputChecked;
+      console.log(inputChecked);
+      console.log(formElement);
     }
+  });
+
+  //console.log(formElement);
+  //console.log(Object.keys(formElement));
+
+  if (ctrlForm(formElement)) {
+    console.log(formElement);
+    // && radio(radioInputs)
+    closeModal();
+    afficherToast();
+    reset();
+  } else {
+    //errorMessages();
+    alertError();
+  }
 });
-   
+
+//function errorMessages() {} //errorFirst, errorLast, errorMail, errorBirthdate, errorQuantity, errorTournois, errorCGeneral
+
+function reset() {
+  document.getElementById("form").reset();
+}
 
 function afficherToast() {
-    let toast = document.getElementById("toast");
-    toast.classList.remove("hidden");
-    toast.classList.add("show");
+  let toast = document.getElementById("toast");
+  toast.classList.remove("hidden");
+  toast.classList.add("show");
 
-    setTimeout(function() {
-        toast.classList.remove("show");
-        toast.classList.add("hidden");
-    }, 3000); // Affiche le toast pendant 3 secondes
+  setTimeout(function () {
+    toast.classList.remove("show");
+    toast.classList.add("hidden");
+  }, 3000); // Affiche le toast pendant 3 secondes
 }
 
 function alertError() {
-    let alert = document.getElementById("alert");
-    alert.classList.remove("hidden");
-    alert.classList.add("show");
+  let alert = document.getElementById("alert");
+  alert.classList.remove("hidden");
+  alert.classList.add("show");
 
-    setTimeout(function() {
-        alert.classList.remove("show");
-        alert.classList.add("hidden");
-    }, 3000); // Affiche le toast pendant 3 secondes
+  setTimeout(function () {
+    alert.classList.remove("show");
+    alert.classList.add("hidden");
+  }, 3000); // Affiche le toast pendant 3 secondes
 }
 
 function ctrlForm(formElement) {
-    //FIRST NAME
-    if(formElement.first.length < 2) {
-      console.log('erreur first');
-      errorFirst = true;
-    } else {
-      errorFirst = false;
-    }
-    
-    // LAST NAME
-    if(formElement.last.length < 2) {
-      errorLast = true;
-      console.log('erreur last');
-    } else {
-      errorLast = false;
-    }
-  
-    // MAIL
-    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formElement.email)) {
-      errorMail = true;
-      console.log('erreur mail');
-    } else {
-      errorMail = false;
-    }
-    
-    // BIRTHDATE 
-    if(isNaN(new Date(formElement.birthdate))) {
-      errorbirthdate = true;
-      console.log('erreur annif');
-    } else {
-      errorbirthdate = false;
-    }
-  
-    // QUANTITY
-    if(formElement.quantity === null && isNaN(formElement.quantity)) {
-      console.log('erreur quantity value');
-    }
-
-  // voir pour modifier avec forEach et map 
-
-    // RADIO BTN
-    if(location1.checked === true) {
-        formElement.location = location1.value;
-        errorTournois = false;
-    } else if(location2.checked === true) {
-        formElement.location = location2.value;
-        errorTournois = false;
-    } else if(location3.checked === true) {
-        formElement.location = location3.value;
-        errorTournois = false;
-    } else if(location4.checked === true) {
-        formElement.location = location4.value;
-        errorTournois = false;
-    } else if(location5.checked === true) {
-        formElement.location = location5.value;
-        errorTournois = false;
-    } else if(location6.checked === true) {
-        formElement.location = location6.value;
-        errorTournois = false;
-    } else {
-        errorTournois = true;
-    }
-  
-    // CHECKBOX CONDITION GENERALE
-    if(checkbox1.checked === true) {
-      errorCGeneral = false;
-    } else {
-      errorCGeneral = true;
-    }
-
-    // CHECKBOX INFORMATION
-    if(checkbox2.checked === true) {
-        newEvent = true;
-    } else if(checkbox2) {
-        newEvent = false;
-    }
-    // Message validation 
-    if(newEvent) {
-        document.getElementById('newEvent').innerHTML = "Nous ne manquerons pas de vous tenir informé des prochains évènements !";
-    } else {
-        document.getElementById('newEvent').innerHTML = "";
-    }
-
-    // Error Message
-    if(errorFirst) {
-      document.getElementById('errorFirst').innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
-      return false;
-    } else {
-      document.getElementById('errorFirst').innerHTML = '';
-    }
-  
-    if(errorLast) {
-      document.getElementById('errorLast').innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
-      return false;
-    } else {
-      document.getElementById('errorLast').innerHTML = '';
-      
-    }
-  
-    if(errorMail) {
-      document.getElementById('errorMail').innerHTML = 'Veuillez corriger votre adresse mail';
-      return false;
-    } else {
-      document.getElementById('errorMail').innerHTML = '';
-      
-    }
-  
-    if(errorbirthdate) {
-      document.getElementById('errorBirthdate').innerHTML = 'Veuillez renseigner votre date de naissance';
-      return false;
-    } else {
-      document.getElementById('errorBirthdate').innerHTML = '';
-      
-    }
-  
-    if(errorTournois) {
-      document.getElementById('errorTournois').innerHTML = 'Veuillez choisir le tournois auquel vous souhaitez participer';
-      return false;
-    } else {
-      document.getElementById('errorTournois').innerHTML = '';
-      
-    }
-  
-    if(errorCGeneral) {
-      document.getElementById('errorCGeneral').innerHTML = 'Veuillez accepter nos conditions générales';
-      return false;
-    } else {
-      document.getElementById('errorCGeneral').innerHTML = '';
-    }
-  
-    return true;
-    
+  //FIRST NAME
+  if (formElement.first.length < 2) {
+    console.log("erreur first");
+    error = true;
+  } else {
+    error = false;
   }
-  
+
+  // LAST NAME
+  if (formElement.last.length < 2) {
+    error = true;
+    console.log("erreur last");
+  } else {
+    error = false;
+  }
+
+  // MAIL
+  if (
+    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formElement.email)
+  ) {
+    error = true;
+    console.log("erreur mail");
+  } else {
+    error = false;
+  }
+
+  // BIRTHDATE
+  if (isNaN(new Date(formElement.birthdate))) {
+    error = true;
+    console.log("erreur annif");
+  } else {
+    error = false;
+  }
+
+  // QUANTITY
+  if (formElement.quantity === null && isNaN(formElement.quantity)) {
+    console.log("erreur quantity value");
+  }
+
+  // RADIO BTN
+
+  // CHECKBOX CONDITION GENERALE
+  if (checkbox1.checked === true) {
+    error = false;
+  } else {
+    error = true;
+  }
+
+  // CHECKBOX INFORMATION
+  if (checkbox2.checked === true) {
+    newEvent = true;
+  } else if (checkbox2) {
+    newEvent = false;
+  }
+
+  return true;
+}
